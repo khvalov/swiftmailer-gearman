ad5b21597a87f5874d5ab40bb96adeb29aa1454e
khvalov <khvalov@tut.by>
2018-01-08 14:13:02 -0500

    Create Mailer.php

---
+++ Mailer.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace khvalov\swiftmailer;
+
+use Yii;
+use yii\base\InvalidConfigException;
+use yii\swiftmailer;
+
+class Mailer extends BaseMailer
+{
+
+    public function sendBackground($message){
+
+        $swiftmailer=$this->getSwiftMailer();
+
+        \Yii::$app->gearman->getDispatcher()->background('mailer', new \shakura\yii2\gearman\JobWorkload([
+            'params' => [
+                'mailer'  => $swiftmailer,
+                'message' =>$message
+            ]
+        ]));
+
+
+        return true;
+    
+    }
+    
